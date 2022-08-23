5c4a3a709a44d93207b65049f002da109950baa2
wenping <wenpingwu9711@gmail.com>
2022-08-23 20:08:32 +0800

    completed create note modal.

--- components/CreateNoteModal.tsx
+++ components/CreateNoteModal.tsx
@@ -1,15 +1,16 @@
-import {
-  View,
-  Text,
-  Modal,
-  SafeAreaView,
-  Pressable,
-  TouchableHighlight,
-} from "react-native";
-import React from "react";
-import { AntDesign } from "@expo/vector-icons";
+import { View, TextInput, Text, Modal, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { AntDesign, Entypo } from "@expo/vector-icons";
+import dayjs from "dayjs";
 
 const CreateNoteModal = ({ isVisible, onClosePress }: any) => {
+  const [time, setTime] = useState<string>();
+  const [text, setText] = useState<string>("");
+
+  useEffect(() => {
+    const t = dayjs().format("MM/DD HH:mm");
+    setTime(t.toString());
+  }, []);
   return (
     <Modal animationType="slide" visible={isVisible}>
       <SafeAreaView style={{ margin: 20 }}>
@@ -17,6 +18,7 @@ const CreateNoteModal = ({ isVisible, onClosePress }: any) => {
           style={{
             flexDirection: "row",
             alignItems: "center",
+            paddingVertical: 12,
           }}
         >
           <AntDesign
@@ -25,12 +27,42 @@ const CreateNoteModal = ({ isVisible, onClosePress }: any) => {
             color="black"
             onPress={onClosePress}
           />
-          {/* <View style={{ flexDirection: "row", marginLeft: "auto" }}>
-            <AntDesign name="close" size={24} color="black" />
-            <AntDesign name="close" size={24} color="black" />
-          </View> */}
+          <AntDesign
+            name="setting"
+            size={20}
+            color="black"
+            style={{ marginLeft: "auto" }}
+          />
+          <AntDesign
+            name="sharealt"
+            size={20}
+            color="black"
+            style={{ marginHorizontal: 20 }}
+          />
+          <Entypo name="dots-three-vertical" size={20} color="black" />
         </View>
-        <Text>CreateNoteModal</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginVertical: 10,
+          }}
+        >
+          <Text style={{ color: "gray" }}>{time}</Text>
+          <View style={{ marginHorizontal: 10, alignItems: "center" }}>
+            <Text style={{ color: "gray", textAlignVertical: "center" }}>
+              |
+            </Text>
+          </View>
+          <Text style={{ color: "gray" }}>{text?.length}字</Text>
+        </View>
+        <TextInput
+          placeholder="text something...."
+          value={text}
+          onChangeText={(text) => setText(text)}
+          keyboardAppearance="default"
+          autoFocus
+        />
       </SafeAreaView>
     </Modal>
   );
